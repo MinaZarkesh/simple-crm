3651688ff3d79880073dc4ea411db104e8012061
Mina <mina_zarkesh@gmx.de>
2024-03-18 23:52:21 +0100

    update sync statement in Backend delete Witness

--- src/app/witness/dialogs/dialog-delete-witness/dialog-delete-witness.component.ts
+++ src/app/witness/dialogs/dialog-delete-witness/dialog-delete-witness.component.ts
@@ -1,31 +1,57 @@
 import { Component } from '@angular/core';
 import { Witness } from '../../../../models/witness.class';
 import { firebaseService } from '../../../firebase-services/firebase.service';
-import { MatDialogRef, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import {
+  MatDialogRef,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 import { NgIf } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { Statement } from '../../../../models/statement.class';
 
 @Component({
   selector: 'app-dialog-delete-witness',
   standalone: true,
-  imports: [NgIf, MatDialogTitle, MatDialogContent, MatDialogActions, MatButtonModule, MatDialogClose],
+  imports: [
+    NgIf,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatButtonModule,
+    MatDialogClose,
+  ],
   templateUrl: './dialog-delete-witness.component.html',
-  styleUrl: './dialog-delete-witness.component.scss'
+  styleUrl: './dialog-delete-witness.component.scss',
 })
 export class DialogDeleteWitnessComponent {
-witness!: Witness;
-witnessId!: string;
-index: number = -1;
-loading: boolean = false;
-witnesses: Witness[] = [];
+  witness!: Witness;
+  witnessId!: string;
+  index: number = -1;
+  loading: boolean = false;
+  witnesses: Witness[] = [];
+  filteredStatements: Statement[] = [];
 
-constructor(public dialogRef: MatDialogRef<DialogDeleteWitnessComponent>, private fireService: firebaseService) {}
+  constructor(
+    public dialogRef: MatDialogRef<DialogDeleteWitnessComponent>,
+    private fireService: firebaseService
+  ) {}
 
-async deleteWitness() {
-  this.loading = true;
-  await this.fireService.deleteSingleWitness(this.witnessId);
-  this.loading = false;
-  this.dialogRef.close();
-  window.location.href = '/witnesses';
-}
+  async deletefilteredStatement() {
+    for (let i = 0; i < this.witness.statements.length; i++) {
+      let stmt = this.witness.statements[i];
+      await this.fireService.deleteSingleStatement(stmt);
+    }
+  }
+
+  async deleteWitness() {
+    await this.deletefilteredStatement();
+    this.loading = true;
+    await this.fireService.deleteSingleWitness(this.witnessId);
+    this.loading = false;
+    this.dialogRef.close();
+    window.location.href = '/witnesses';
+  }
 }
