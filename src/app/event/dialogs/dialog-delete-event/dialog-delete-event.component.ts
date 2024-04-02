5f4d8006d8103ea2e32803174a8da43571c37088
Mina <mina_zarkesh@gmx.de>
2024-04-02 18:05:13 +0200

    fixed deleteEvent

--- src/app/event/dialogs/dialog-delete-event/dialog-delete-event.component.ts
+++ src/app/event/dialogs/dialog-delete-event/dialog-delete-event.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { Witness } from '../../../../models/witness.class';
-import { firebaseService } from '../../../firebase-services/firebase.service';
-import {
-  MatDialogRef,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogTitle,
-} from '@angular/material/dialog';
 import { NgIf } from '@angular/common';
+
+import { MatDialogRef, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+
+import { firebaseService } from '../../../firebase-services/firebase.service';
 import { Statement } from '../../../../models/statement.class';
 import { Event } from '../../../../models/event.class';
+
 @Component({
   selector: 'app-dialog-delete-event',
   standalone: true,
@@ -27,6 +23,7 @@ import { Event } from '../../../../models/event.class';
   styleUrl: './dialog-delete-event.component.scss',
 })
 export class DialogDeleteEventComponent {
+
   event!: Event;
   eventId!: string;
   statements!: Statement[];
@@ -40,38 +37,25 @@ export class DialogDeleteEventComponent {
   async deletefilteredStatement() {
     this.statements = this.fireService.statements;
     let tempStatements: any[] = [];
+
     this.statements.find((element) => {
       if (element.event == this.event.docId) {
         tempStatements.push(element);
       }
     });
-    // let stmt = this.statements[i];
-    console.log(
-      'filteredStatements: ',
-      this.statements,
-      this.event.docId,
-      tempStatements
-    );
 
     await tempStatements.forEach((stmt) => {
       this.fireService.deleteSingleStatement(stmt.docId);
-      console.log('deleted: ', stmt.docId);
     });
-    console.log(
-      'updateStatements: ',
-      this.fireService.statements,this.fireService.events, this.fireService.witnesses
-    );
   }
 
   async deleteEvent() {
     this.loading = true;
-    console.log("delete id: ",this.eventId);
+
     await this.deletefilteredStatement();
-    if(this.event.docId){
-      this.eventId = this.event.docId
-    }
+    if(this.event.docId) this.eventId = this.event.docId;
     await this.fireService.deleteSingleEvent(this.eventId);
+
     this.loading = false;
-    console.log('deleted: ', this.event, this.event.docId);
   }
 }
